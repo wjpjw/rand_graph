808a11621a70ebe6d35018c94a06f0e58e6c4d6b
Sam Seifert <seifes1@gmail.com>
2015-11-05 22:28:44 -0500

    added constraint each node must be connected to 5 others

--- graph_gen_2/graph.c
+++ graph_gen_2/graph.c
@@ -1,4 +1,5 @@
 #include "graph.h"
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -24,6 +25,7 @@ void                   add_edge(graph* g, int v1, int v2){
 void                   generate_rand_graph(graph* g, int starting_count){
     
     int * number_edges = (int*) malloc(sizeof(int) * g->nr_vertices);
+    int * current_edges = (int*) malloc(sizeof(int) * starting_count);
     int total_number_edges = 0;
     
     for (int i=0; i<g->nr_vertices; i++)
@@ -41,19 +43,55 @@ void                   generate_rand_graph(graph* g, int starting_count){
             total_number_edges++;
         }
     }
-
+    
+    
+    
+    
     for (int i=starting_count;i<g->nr_vertices;i++)
     {
-        for(int j=0; j < i;j++)
+        int number_edges_connected_to = 0;
+        
+        int max_number_viable_edges = total_number_edges * 2;
+        
+        while (number_edges_connected_to != starting_count)
         {
-            if(urand() * total_number_edges < number_edges[j])
+            int ne = (int)round(urand() * max_number_viable_edges);
+            
+            for (int j = 0; j < i; j++)
             {
-                add_edge(g, i, j);
-                number_edges[i]++;
-                number_edges[j]++;
-                total_number_edges++;
+                int not_already_connected_to_this_edge = 1;
+                
+                for (int dex = 0; dex < number_edges_connected_to; dex++)
+                {
+                    if (j == current_edges[dex])
+                    {
+                        not_already_connected_to_this_edge = 0;
+                        break;
+                    }
+                }
+
+                if (not_already_connected_to_this_edge == 1)
+                {
+                    ne -= number_edges[j];
+                    if (ne <= 0)
+                    {
+                        max_number_viable_edges -= number_edges[j]; // Can't select this edge no more!
+                        current_edges[number_edges_connected_to] = j; // Remember We've Already connected node i to j
+                        number_edges_connected_to ++; // Count number of nodes we've connected i too                        
+
+                        add_edge(g, i, j);
+                        number_edges[i]++;
+                        number_edges[j]++;
+                        total_number_edges++;
+
+                        break;
+                    }
+                }
+
+                
+                if (j == i - 1) printf("GOT TO END\n");
             }
-        }  
+        }
     }
     
     free(number_edges);
